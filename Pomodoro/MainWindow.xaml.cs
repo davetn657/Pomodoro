5c069222a9ee9444c1082be29fa14bb71501fa33
Dave Ngo <davetn657@gmail.com>
2025-06-28 11:32:50 -0400

    Adding timer functionality
    - Added functionality for Rest Timer
    - Added new Window for Rest Timer
    - Added Working progress bar for Rest Timer
    - Added stop button and stop functionality for Work Timer
    
    TO-DO:
    - Add ability for program to cycle between work and rest timers autonomously
    - Add functioning tip buttons
    - Add decals (i.e. loading icons) to make it look good

--- Pomodoro/MainWindow.xaml.cs
+++ Pomodoro/MainWindow.xaml.cs
@@ -17,46 +17,104 @@ namespace Pomodoro
     /// </summary>
     public partial class MainWindow : Window
     {
-        public int _workTime = 10;
-        public int _restTime = 5;
-        public int _workCounter = 0;
+        private int _workTime = 10;
+        private int _restTime = 5;
+        private int _workCounter = 0;
         public int _restCounter = 0;
         DispatcherTimer _dispatcherTimer;
 
         public MainWindow()
         {
             InitializeComponent();
+
+            // Set up the NotifyIcon so the application can minimize to the system tray
+            System.Windows.Forms.NotifyIcon notifyIcon = new System.Windows.Forms.NotifyIcon();
+            notifyIcon.Icon = new System.Drawing.Icon(@"..\Media\Icons\Main.ico");
+            notifyIcon.Visible = true;
+            notifyIcon.DoubleClick += 
+                delegate(object sender, EventArgs e)
+                {
+                    this.Show();
+                    this.WindowState = WindowState.Normal;
+                };
+
+        }
+
+        // This method is called when the window state changes, specifically when it is minimized
+        // It hides the window instead of closing it.
+        protected override void OnStateChanged(EventArgs e)
+        {
+            if (WindowState == System.Windows.WindowState.Minimized)
+                this.Hide();
+
+            base.OnStateChanged(e);
         }
 
         private void startbtn_Click(object sender, RoutedEventArgs e)
         {
-            //start work timer and minimize
-            //when work timer is over open new form
             _workTime = Int32.Parse(worktxt.Text);
             _restTime = Int32.Parse(resttxt.Text);
 
             _workCounter = _workTime;
             _restCounter = _restTime;
 
+            StartWorkTimer();
+        }
+
+        private void stopbtn_Click(object sender, RoutedEventArgs e)
+        {
+            StopWorkTimer();
+        }
+
+        public void StartWorkTimer()
+        {
+            startbtn.IsEnabled = false;
+            startbtn.Visibility = Visibility.Hidden;
+
+            stopbtn.IsEnabled = true;
+            stopbtn.Visibility = Visibility.Visible;
+
+            //start work timer and minimize
+            //when work timer is over open new form
             _dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
             _dispatcherTimer.Tick += new EventHandler(Timer_Tick);
-            _dispatcherTimer.Interval = TimeSpan.FromMinutes(1);
+            _dispatcherTimer.Interval = TimeSpan.FromSeconds(1); //Change to minutes
             _dispatcherTimer.Start();
 
+            this.WindowState = WindowState.Minimized;
+        }
+
+        public void StopWorkTimer()
+        {
+
+            startbtn.IsEnabled = true;
+            startbtn.Visibility = Visibility.Visible;
+
+            stopbtn.IsEnabled = false;
+            stopbtn.Visibility = Visibility.Hidden;
+
+            if (_dispatcherTimer != null)
+            {
+                _dispatcherTimer.Stop();
+            }
         }
 
         private void Timer_Tick(object sender, EventArgs e)
         {
             // Logic to handle timer tick
             // This could include updating a label with the remaining time
-            worktxt.Text = _workCounter.ToString();
             _workCounter--;
 
             if (_workCounter == 0)
             {
-                System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"C:\Projects\Pomodoro\Pomodoro\Media\Sounds\blip.wav");
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"..\Media\Sounds\blip.wav");
                 player.Play();
                 _dispatcherTimer.Stop();
+
+                // Open rest timer
+                RestTimeWindow restTimeWindow = new RestTimeWindow(_restCounter);
+                restTimeWindow.Owner = this;
+                restTimeWindow.Show();
             }
         }
     }
---
+++ Pomodoro/RestTimeWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Pomodoro
+{
+    /// <summary>
+    /// Interaction logic for RestTimeWindow.xaml
+    /// </summary>
+    public partial class RestTimeWindow : Window
+    {
+        DispatcherTimer _dispatcherTimer;
+        public int _restCounter = 0;
+        public string _restTime { get; set; }
+
+        public RestTimeWindow(int restTime)
+        {
+            _restCounter = restTime;
+            _restTime = ($"0:{restTime}:0");
+
+            InitializeComponent();
+        }
+       
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            _restCounter--;
+
+            if(_restCounter == 0)
+            {
+                //Play sound indicating end of rest time
+                //Stop timer
+                //Start Work Timer
+                //Close this window
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"..\Media\Sounds\blip.wav");
+                player.Play();
+
+                StopRestTimer();
+
+            }
+        }
+
+        private void StartRestTimer()
+        {
+            _dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+            _dispatcherTimer.Tick += new EventHandler(Timer_Tick);
+            _dispatcherTimer.Interval = TimeSpan.FromSeconds(1); //Change to minutes
+            _dispatcherTimer.Start();
+        }
+
+        private void StopRestTimer()
+        {
+            _dispatcherTimer.Stop();
+            this.Close();
+        }
+
+        private void tiponebtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
